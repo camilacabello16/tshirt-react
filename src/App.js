75d92088e0641831bfacc19e0eefe8f6efe9b7f2
hieubd <buiduchieuuet16@gmail.com>
2023-02-23 15:58:02 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { fabric } from "fabric";
+import tingle from "./tingle.min.js";
+import productConfig from "./product.config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div id="app">
+            <div id="designer-area">
+                <div id="side-switcher">
+                    <div className="button-group"></div>
+                </div>
+
+                <div className="designer">
+                    <div id="bg-color"></div>
+                    <div id="canvases"></div>
+                </div>
+            </div>
+
+            <aside>
+                <section>
+                    <h3>Colors:</h3>
+                    <div id="colors"></div>
+                </section>
+                <section>
+                    <h3>Upload</h3>
+                    <input type="file" name="uploadart" style={{ display: "none" }} id="uploadart" />
+                    <label htmlFor="uploadart">&nbsp;
+                        <a className="button">Upload Artwork</a>
+                    </label>
+                </section>
+
+                <section>
+                    <h3>Export</h3>
+                    <a id="exportart" className="button outline small">Export Art</a>
+                    <a id="exportmockup" className="button outline small">Export Flat Mockup</a>
+                    <a id="generatemockups" className="button outline small">Generate Mockups</a>
+                </section>
+            </aside>
+        </div>
+    );
 }
 
 export default App;
